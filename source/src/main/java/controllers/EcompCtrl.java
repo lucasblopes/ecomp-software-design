ea1064886d5336f03976d183328842763fc88048
yago yudi <yagofuruta@protonmail.com>
2024-12-09 09:37:59 -0300

    feat: .editorconfig

--- source/src/main/java/controllers/EcompCtrl.java
+++ source/src/main/java/controllers/EcompCtrl.java
@@ -7,55 +7,55 @@ import views.EcompView;
 // Facade Pattern
 public class EcompCtrl {
 
-  private EcompView view;
-  private ProjetoRepository projetos;
-  private MembroRepository membros;
+	private EcompView view;
+	private ProjetoRepository projetos;
+	private MembroRepository membros;
 
-  public EcompCtrl() {
-    this.view = new EcompView();
-    this.projetos = new ProjetoRepository();
-    this.membros = new MembroRepository();
-  }
+	public EcompCtrl() {
+		this.view = new EcompView();
+		this.projetos = new ProjetoRepository();
+		this.membros = new MembroRepository();
+	}
 
-  // Método que processa a escolha do usuário
-  public void iniciar() {
-    int opcao;
-    do {
-      // Exibe o menu e lê a opção do usuário via view
-      view.limparTela();
-      view.showMenu();
-      opcao = view.getInputInt();
-      view.limparTela();
+	// Método que processa a escolha do usuário
+	public void iniciar() {
+		int opcao;
+		do {
+			// Exibe o menu e lê a opção do usuário via view
+			view.limparTela();
+			view.showMenu();
+			opcao = view.getInputInt();
+			view.limparTela();
 
-      // Processa a opção selecionada
-      switch (opcao) {
-        case 1:
-          new CadastrarProjetoCtrl().cadastrarProjeto(projetos);
-          break;
-        case 2:
-          new CadastrarEtapasCtrl().cadastrarEtapa(projetos);
-          break;
-        case 3:
-          new AdicionarDevsCtrl().adicionarDevs();
-          break;
-        case 4:
-          new CadastrarAtividadeCtrl().cadastrarAtividade();
-          break;
-        case 5:
-          new CadastrarNotasFiscaisCtrl().cadastrarNotasFiscais();
-          break;
-        case 6:
-          new CadastrarMembroCtrl().cadastrarMembro(membros);
-          break;
-        case 7:
-          new GerarRelatorioCtrl().gerarRelatorio();
-          break;
-        case 0:
-          view.showSuccess("Saindo do sistema... Até logo!");
-          break;
-        default:
-          view.showError("Opção inválida. Tente novamente.");
-      }
-    } while (opcao != 0);
-  }
+			// Processa a opção selecionada
+			switch (opcao) {
+				case 1:
+					new CadastrarProjetoCtrl().cadastrarProjeto(projetos);
+					break;
+				case 2:
+					new CadastrarEtapasCtrl().cadastrarEtapa(projetos);
+					break;
+				case 3:
+					new AdicionarDevsCtrl().adicionarDevs();
+					break;
+				case 4:
+					new CadastrarAtividadeCtrl().cadastrarAtividade();
+					break;
+				case 5:
+					new CadastrarNotasFiscaisCtrl().cadastrarNotasFiscais();
+					break;
+				case 6:
+					new CadastrarMembroCtrl().cadastrarMembro(membros);
+					break;
+				case 7:
+					new GerarRelatorioCtrl().gerarRelatorio();
+					break;
+				case 0:
+					view.showSuccess("Saindo do sistema... Até logo!");
+					break;
+				default:
+					view.showError("Opção inválida. Tente novamente.");
+			}
+		} while (opcao != 0);
+	}
 }
