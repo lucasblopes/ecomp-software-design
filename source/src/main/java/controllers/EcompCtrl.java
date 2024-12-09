ee4c68a00b22341e3712b7be0ecb2d265dfaaca5
Eric Kivel <ericvkivel1@gmail.com>
2024-12-09 09:06:28 -0300

    feat: add cadastrarMembro action

--- source/src/main/java/controllers/EcompCtrl.java
+++ source/src/main/java/controllers/EcompCtrl.java
@@ -1,58 +1,61 @@
 package controllers;
 
+import repositories.MembroRepository;
 import repositories.ProjetoRepository;
 import views.EcompView;
 
 // Facade Pattern
 public class EcompCtrl {
 
-    private EcompView view;
-    private ProjetoRepository projetos;
+  private EcompView view;
+  private ProjetoRepository projetos;
+  private MembroRepository membros;
 
-    public EcompCtrl() {
-        this.view = new EcompView();
-        this.projetos = new ProjetoRepository();
-    }
+  public EcompCtrl() {
+    this.view = new EcompView();
+    this.projetos = new ProjetoRepository();
+    this.membros = new MembroRepository();
+  }
 
-    // Método que processa a escolha do usuário
-    public void iniciar() {
-        int opcao;
-        do {
-            // Exibe o menu e lê a opção do usuário via view
-            view.limparTela();
-            view.showMenu();
-            opcao = view.getInputInt();
-            view.limparTela();
+  // Método que processa a escolha do usuário
+  public void iniciar() {
+    int opcao;
+    do {
+      // Exibe o menu e lê a opção do usuário via view
+      view.limparTela();
+      view.showMenu();
+      opcao = view.getInputInt();
+      view.limparTela();
 
-            // Processa a opção selecionada
-            switch (opcao) {
-                case 1:
-                    new CadastrarProjetoCtrl().cadastrarProjeto(projetos);
-                    break;
-                case 2:
-                    new CadastrarEtapasCtrl().cadastrarEtapa(projetos);
-                    break;
-                case 3:
-                    new AdicionarDevsCtrl().adicionarDevs();
-                    break;
-                case 4:
-                    new CadastrarAtividadeCtrl().cadastrarAtividade();
-                    break;
-                case 5:
-                    new CadastrarNotasFiscaisCtrl().cadastrarNotasFiscais();
-                    break;
-                case 6:
-                    new CadastrarMembroCtrl().cadastrarMembro();
-                    break;
-                case 7:
-                    new GerarRelatorioCtrl().gerarRelatorio();
-                    break;
-                case 0:
-                    view.showSuccess("Saindo do sistema... Até logo!");
-                    break;
-                default:
-                    view.showError("Opção inválida. Tente novamente.");
-            }
-        } while (opcao != 0);
-    }
+      // Processa a opção selecionada
+      switch (opcao) {
+        case 1:
+          new CadastrarProjetoCtrl().cadastrarProjeto(projetos);
+          break;
+        case 2:
+          new CadastrarEtapasCtrl().cadastrarEtapa(projetos);
+          break;
+        case 3:
+          new AdicionarDevsCtrl().adicionarDevs();
+          break;
+        case 4:
+          new CadastrarAtividadeCtrl().cadastrarAtividade();
+          break;
+        case 5:
+          new CadastrarNotasFiscaisCtrl().cadastrarNotasFiscais();
+          break;
+        case 6:
+          new CadastrarMembroCtrl().cadastrarMembro(membros);
+          break;
+        case 7:
+          new GerarRelatorioCtrl().gerarRelatorio();
+          break;
+        case 0:
+          view.showSuccess("Saindo do sistema... Até logo!");
+          break;
+        default:
+          view.showError("Opção inválida. Tente novamente.");
+      }
+    } while (opcao != 0);
+  }
 }
---
+++ source/src/main/java/models/MembroBuilder.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class MembroBuilder {
+  private String nome;
+  private String cpf;
+  private String rg;
+  private String email;
+  private String telefone;
+  private String cargo;
+  private String diretoria;
+  private String curso;
+  private LocalDate dataNascimento;
+
+  public MembroBuilder() {
+    // Inicializa os valores caso necessário
+  }
+
+  public MembroBuilder nome(String nome) {
+    this.nome = nome;
+    return this;
+  }
+
+  public MembroBuilder cpf(String cpf) {
+    this.cpf = cpf;
+    return this;
+  }
+
+  public MembroBuilder rg(String rg) {
+    this.rg = rg;
+    return this;
+  }
+
+  public MembroBuilder email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public MembroBuilder telefone(String telefone) {
+    this.telefone = telefone;
+    return this;
+  }
+
+  public MembroBuilder cargo(String cargo) {
+    this.cargo = cargo;
+    return this;
+  }
+
+  public MembroBuilder diretoria(String diretoria) {
+    this.diretoria = diretoria;
+    return this;
+  }
+
+  public MembroBuilder curso(String curso) {
+    this.curso = curso;
+    return this;
+  }
+
+  public MembroBuilder dataNascimento(LocalDate dataNascimento) {
+    this.dataNascimento = dataNascimento;
+    return this;
+  }
+
+  // Método para construir o objeto Membro
+  public Membro build() {
+    Membro membro = new Membro(nome, cpf, rg, email, telefone, cargo, diretoria, curso, dataNascimento);
+
+    // Valida os campos do membro
+    List<String> erros = membro.validarCampos();
+    if (!erros.isEmpty()) {
+      System.out.println("Erro de validação: " + String.join(", ", erros));
+      return null;
+    }
+
+    return membro;
+  }
+}
