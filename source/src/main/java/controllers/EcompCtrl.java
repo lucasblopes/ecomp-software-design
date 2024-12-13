34b9d4f453f9f7ecc17e7828d6202c39bbff25db
yago yudi <yagofuruta@protonmail.com>
2024-12-13 15:12:42 -0300

    Autenticação básica

--- source/src/main/java/controllers/EcompCtrl.java
+++ source/src/main/java/controllers/EcompCtrl.java
@@ -5,77 +5,97 @@ import views.EcompView;
 
 // Facade Pattern
 public class EcompCtrl {
-    private EcompView view;
-    private ProjectRepository projRepo;
-    private MemberRepository memberRepo;
-    private ActivityRepository activityRepo;
-    private DevRepository devRepo;
+	private EcompView view;
+	private ProjectRepository projRepo;
+	private MemberRepository memberRepo;
+	private ActivityRepository activityRepo;
+	private DevRepository devRepo;
 
-    private RegisterProjectCtrl regProjCtrl;
-    private RegisterStageCtrl stageCtrl;
-    private RegisterActivityCtrl activityCtrl;
-    private RegisterInvoiceCtrl invoiceCtrl;
-    private RegisterMemberCtrl memberCtrl;
-    private GenerateReportCtrl genRepCtrl;
-    private DevCtrl devCtrl;
+	private RegisterProjectCtrl regProjCtrl;
+	private RegisterStageCtrl stageCtrl;
+	private RegisterActivityCtrl activityCtrl;
+	private RegisterInvoiceCtrl invoiceCtrl;
+	private RegisterMemberCtrl memberCtrl;
+	private GenerateReportCtrl genRepCtrl;
+	private DevCtrl devCtrl;
 
-    public EcompCtrl() {
-        // Inicialização dos repositórios
-        this.view = new EcompView();
-        this.projRepo = new ProjectRepository();
-        this.memberRepo = new MemberRepository();
-        this.activityRepo = new ActivityRepository();
+	private boolean isAuthenticated = false;
 
-        // Inicialização dos controladores
-        this.regProjCtrl = new RegisterProjectCtrl(this.projRepo);
-        this.stageCtrl = new RegisterStageCtrl(this.projRepo);
-        this.activityCtrl = new RegisterActivityCtrl(this.activityRepo, this.memberRepo);
-        this.memberCtrl = new RegisterMemberCtrl(this.memberRepo);
-        this.invoiceCtrl = new RegisterInvoiceCtrl();
-        this.genRepCtrl = new GenerateReportCtrl();
-        this.devCtrl = new DevCtrl(this.projRepo, this.memberRepo, this.devRepo);
-    }
+	public EcompCtrl() {
+		// Inicialização dos repositórios
+		this.view = new EcompView();
+		this.projRepo = new ProjectRepository();
+		this.memberRepo = new MemberRepository();
+		this.activityRepo = new ActivityRepository();
 
-    public void start() {
-        int option;
-        do {
-            view.clearScreen();
-            view.showMenu();
-            option = view.getInputInt();
-            view.clearScreen();
+		// Inicialização dos controladores
+		this.regProjCtrl = new RegisterProjectCtrl(this.projRepo);
+		this.stageCtrl = new RegisterStageCtrl(this.projRepo);
+		this.activityCtrl = new RegisterActivityCtrl(this.activityRepo, this.memberRepo);
+		this.memberCtrl = new RegisterMemberCtrl(this.memberRepo);
+		this.invoiceCtrl = new RegisterInvoiceCtrl();
+		this.genRepCtrl = new GenerateReportCtrl();
+		this.devCtrl = new DevCtrl(this.projRepo, this.memberRepo, this.devRepo);
+	}
 
-            processOption(option);
-        } while (option != 0);
-    }
+	public void start() {
+		int option;
+		do {
+			view.clearScreen();
+			view.showMenu();
+			option = view.getInputInt();
+			view.clearScreen();
 
-    private void processOption(int option) {
-        switch (option) {
-            case 1:
-                regProjCtrl.registerProject();
-                break;
-            case 2:
-                stageCtrl.registerStage();
-                break;
-            case 3:
-                devCtrl.manage();
-                break;
-            case 4:
-                activityCtrl.registerActivity();
-                break;
-            case 5:
-                invoiceCtrl.registerInvoice();
-                break;
-            case 6:
-                memberCtrl.registerMember();
-                break;
-            case 7:
-                genRepCtrl.generateReport();
-                break;
-            case 0:
-                view.showSuccess("Saindo do sistema... Até logo!");
-                break;
-            default:
-                view.showError("Opção inválida. Tente novamente.");
-        }
-    }
+			processOption(option);
+		} while (option != 0);
+	}
+
+	private void processOption(int option) {
+		switch (option) {
+			case 1:
+				regProjCtrl.registerProject();
+				break;
+			case 2:
+				stageCtrl.registerStage();
+				break;
+			case 3:
+				devCtrl.manage();
+				break;
+			case 4:
+				if (isAuthenticated) {
+					activityCtrl.registerActivity();
+				} else {
+					view.showError("Usuário não autenciado. Por favor, faça login antes de continua");
+				}
+				break;
+			case 5:
+				invoiceCtrl.registerInvoice();
+				break;
+			case 6:
+				memberCtrl.registerMember();
+				break;
+			case 7:
+				genRepCtrl.generateReport();
+				break;
+			case 8:
+				System.out.println("===Login do Sistema===");
+				System.out.printf("Digite o usuário: ");
+				String username = view.getInput();
+				System.out.printf("Digite a senha: ");
+				String password = view.getInput();
+
+				if (username.equals("a") && password.equals("a")) {
+					isAuthenticated = true;
+					view.showSuccess("Login realizado com sucesso!");
+				} else {
+					view.showSuccess("Credenciais inválidas. Tente novamente.");
+				}
+				break;
+			case 0:
+				view.showSuccess("Saindo do sistema... Até logo!");
+				break;
+			default:
+				view.showError("Opção inválida. Tente novamente.");
+		}
+	}
 }
--- source/src/main/java/views/EcompView.java
+++ source/src/main/java/views/EcompView.java
@@ -7,7 +7,7 @@ public class EcompView extends GenericView {
     }
 
     public void showMenu() {
-		System.out.println("=== Sistema Ecomp ===");
+	System.out.println("=== Sistema Ecomp ===");
         System.out.println("1 - Cadastrar Projetos");
         System.out.println("2 - Cadastrar Etapas do Desenvolvimento do Projeto");
         System.out.println("3 - Gerenciar Desenvolvedores");
@@ -15,6 +15,7 @@ public class EcompView extends GenericView {
         System.out.println("5 - Cadastrar Notas Fiscais e Recibos");
         System.out.println("6 - Cadastrar Membro");
         System.out.println("7 - Gerar Relatório");
+        System.out.println("8 - Login");
         System.out.println("0 - Sair");
         System.out.print("Escolha uma opção (1-7): ");
     }
